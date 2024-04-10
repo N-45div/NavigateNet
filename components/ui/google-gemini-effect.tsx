0dcf1ecf73db8dc3c568feb87f83529083dd77e3
Divij <ndivij2004@gmail.com>
2024-04-10 12:41:20 +0530

    Update google-gemini-effect.tsx

--- components/ui/google-gemini-effect.tsx
+++ components/ui/google-gemini-effect.tsx
@@ -44,9 +44,7 @@ export const GoogleGeminiEffect = ({
         {title || `NavigateNet`}
       </p>
       <div className="flex flex-col items-center justify-center h-[40rem]  ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-        The road to freedom starts from here
-      </p>
+     
       <TypewriterEffectSmooth words={words} />
     </div>
    
